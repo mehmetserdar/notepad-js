438bd3fbbc9c09fd61b660bb6f31b474ff5400b1
mehmetserdar <msrdrkskn@gmail.com>
2023-06-17 07:57:16 +0300

    updated edit button and alerts

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var noteKeyPrefix = "note_";
 var colorButtons = document.getElementsByClassName("color-button");
-var feelingButton = document.getElementById('feeling-button');
-var feelingModal = document.getElementById('feeling-modal');
-var feelingChoices = document.getElementsByClassName('feeling-choice');
-var importButton = document.getElementById('import-button');
-var exportButton = document.getElementById('export-button');
-var importFile = document.getElementById('import-file');
+var feelingButton = document.getElementById("feeling-button");
+var feelingModal = document.getElementById("feeling-modal");
+var feelingChoices = document.getElementsByClassName("feeling-choice");
+var importButton = document.getElementById("import-button");
+var exportButton = document.getElementById("export-button");
+var importFile = document.getElementById("import-file");
 
 function handleColorButtonClick(event) {
   var color = event.target.getAttribute("data-color");
@@ -14,14 +14,14 @@ function handleColorButtonClick(event) {
 
 // Generate a unique note ID
 function generateNoteId() {
-  return 'note_' + Date.now();
+  return "note_" + Date.now();
 }
 
 // Function to convert RGB color to hex color
 function rgbToHex(rgb) {
   try {
     // Remove whitespace and extract the RGB values
-    var rgbValues = rgb.replace(/\s/g, '').match(/^rgb\((\d+),(\d+),(\d+)\)$/);
+    var rgbValues = rgb.replace(/\s/g, "").match(/^rgb\((\d+),(\d+),(\d+)\)$/);
 
     // Check if RGB values are valid
     if (rgbValues && rgbValues.length === 4) {
@@ -30,31 +30,32 @@ function rgbToHex(rgb) {
       var b = parseInt(rgbValues[3]);
 
       // Convert each RGB component to hex
-      var hex = '#' + ((r << 16) | (g << 8) | b).toString(16).padStart(6, '0');
+      var hex = "#" + ((r << 16) | (g << 8) | b).toString(16).padStart(6, "0");
 
       return hex;
     } else {
       // Return default hex color (#ffffff) if RGB format is invalid or empty
-      return '#ffffff';
+      return "#ffffff";
     }
   } catch (error) {
-    console.error('Error converting RGB to hex:', error);
-    return '#ffffff';
+    console.error("Error converting RGB to hex:", error);
+    return "#ffffff";
   }
 }
 
 // Attach event listener to feeling button
-feelingButton.addEventListener('click', function() {
-  $('#feeling-modal').modal('show');
+feelingButton.addEventListener("click", function () {
+  $("#feeling-modal").modal("show");
 });
 
 // Attach event listeners to feeling choices
 for (var i = 0; i < feelingChoices.length; i++) {
-  feelingChoices[i].addEventListener('click', function() {
+  feelingChoices[i].addEventListener("click", function () {
     var feelingChoice = this.innerText;
     var today = new Date().toLocaleDateString();
-    document.getElementById('note-content').value = "I'm " + feelingChoice.toLowerCase() + " (" + today + ")";
-    $('#feeling-modal').modal('hide');
+    document.getElementById("note-content").value =
+      "I'm " + feelingChoice.toLowerCase() + " (" + today + ")";
+    $("#feeling-modal").modal("hide");
   });
 }
 
@@ -71,61 +72,54 @@ function createNoteItem(noteId, noteContent, noteColor) {
   li.appendChild(noteContentElement);
 
   var setButton = document.createElement("button");
-setButton.className = "btn btn-edit btn-sm mr-2";
-setButton.innerHTML = "⚙️";
-setButton.addEventListener("click", function () {
-  editDeleteContainer.classList.toggle("hidden");
-});
-
-var editButton = document.createElement("button");
-editButton.className = "btn btn-edit btn-sm mr-2 hide";
-editButton.innerHTML = "✏️";
-editButton.addEventListener("click", function () {
-  document.getElementById("note-content").value = noteContent;
-  document
-    .getElementById("note-content")
-    .setAttribute("data-note-id", noteId);
-});
-
-var deleteButton = document.createElement("button");
-deleteButton.className = "btn btn-edit btn-sm hide text-center";
-deleteButton.innerHTML = "❌";
-deleteButton.addEventListener("click", function () {
-  $('#deleteModal').modal('show');
-  var deleteNoteButton = document.getElementById('deleteNote');
-  deleteNoteButton.addEventListener('click', function () {
-    
-    li.parentNode.removeChild(li);
-    localStorage.removeItem(noteKeyPrefix + noteId);
-    $('#deleteModal').modal('hide');
-    
+  setButton.className = "btn btn-edit btn-sm mr-2";
+  setButton.innerHTML = "⚙️";
+  setButton.addEventListener("click", function () {
+    editDeleteContainer.classList.toggle("hidden");
   });
-});
 
+  var editButton = document.createElement("button");
+  editButton.className = "btn btn-edit btn-sm mr-2 hide";
+  editButton.innerHTML = "✏️";
+  editButton.addEventListener("click", function () {
+    document.getElementById("note-content").value = noteContent;
+    document
+      .getElementById("note-content")
+      .setAttribute("data-note-id", noteId);
+  });
 
+  var deleteButton = document.createElement("button");
+  deleteButton.className = "btn btn-edit btn-sm hide text-center";
+  deleteButton.innerHTML = "🗑️";
+  deleteButton.addEventListener("click", function () {
+    $("#deleteModal").modal("show");
+    var deleteNoteButton = document.getElementById("deleteNote");
+    deleteNoteButton.addEventListener("click", function () {
+      li.parentNode.removeChild(li);
+      localStorage.removeItem(noteKeyPrefix + noteId);
+      $("#deleteModal").modal("hide");
+    });
+  });
 
+  var editDeleteContainer = document.createElement("div");
+  editDeleteContainer.className = "float-right hidden";
+  editDeleteContainer.appendChild(editButton);
+  editDeleteContainer.appendChild(deleteButton);
 
-var editDeleteContainer = document.createElement("div");
-editDeleteContainer.className = "float-right hidden";
-editDeleteContainer.appendChild(editButton);
-editDeleteContainer.appendChild(deleteButton);
+  var setContainer = document.createElement("div");
+  setContainer.className = "float-right";
+  setContainer.appendChild(setButton);
 
-var setContainer = document.createElement("div");
-setContainer.className ="float-right";
-setContainer.appendChild(setButton);
+  li.appendChild(editDeleteContainer);
+  li.appendChild(setContainer);
 
-li.appendChild(editDeleteContainer);
-li.appendChild(setContainer);
-
-return li;
-
+  return li;
 }
 
 function updateNoteColor(color) {
   var noteItem = document.getElementById("note-content");
   noteItem.style.backgroundColor = color;
-  var noteColor =
-      document.getElementById("note-content").style.backgroundColor;
+  var noteColor = document.getElementById("note-content").style.backgroundColor;
   noteColor = rgbToHex(noteColor);
   var noteId = noteItem.getAttribute("data-note-id");
   if (noteId) {
@@ -141,14 +135,13 @@ for (var i = 0; i < colorButtons.length; i++) {
 
 document.getElementById("save-button").addEventListener("click", function () {
   var noteContent = document.getElementById("note-content").value.trim(); // Trim leading and trailing whitespace
-  ;
   if (noteContent.trim() !== "") {
     var noteId = document
       .getElementById("note-content")
       .getAttribute("data-note-id");
     var noteColor =
       document.getElementById("note-content").style.backgroundColor;
-      noteColor = rgbToHex(noteColor);
+    noteColor = rgbToHex(noteColor);
     if (noteId) {
       // Update existing note
       var noteItem = document.getElementById(noteId);
@@ -158,7 +151,6 @@ document.getElementById("save-button").addEventListener("click", function () {
         noteKeyPrefix + noteId,
         JSON.stringify({ content: noteContent, color: noteColor })
       );
-      alert("Note updated successfully!");
       // Refresh the page
       location.reload();
     } else {
@@ -170,20 +162,17 @@ document.getElementById("save-button").addEventListener("click", function () {
         noteKeyPrefix + noteId,
         JSON.stringify({ content: noteContent, color: noteColor })
       );
-      alert("Note saved successfully!");
       // Refresh the page
       location.reload();
     }
     document.getElementById("note-content").value = "";
     document.getElementById("note-content").removeAttribute("data-note-id");
-  } else {
-    alert("Note content cannot be empty!");
   }
 });
 
 // Function to export notes as CSV
 function exportNotesAsCSV() {
-  var csvContent = '';
+  var csvContent = "";
 
   // Add the CSV header
   var header = ["NoteId", "NoteContent", "NoteColor"];
@@ -196,7 +185,7 @@ function exportNotesAsCSV() {
       var noteId = key.substring(noteKeyPrefix.length);
       var noteData = JSON.parse(localStorage.getItem(key));
       var noteContent = noteData.content;
-      var noteColor = noteData.color || '#ffffff'; // Default color if not set
+      var noteColor = noteData.color || "#ffffff"; // Default color if not set
 
       // Convert RGB color value to hex
 
@@ -207,7 +196,7 @@ function exportNotesAsCSV() {
   }
 
   // Create a Blob object with the CSV content
-  var blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+  var blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
 
   // Create a link element and set the Blob object as its href
   var link = document.createElement("a");
@@ -221,52 +210,52 @@ function exportNotesAsCSV() {
 }
 // Function to import notes from CSV
 function importNotesFromCSV(file) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     var reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = function (e) {
       var importedData = e.target.result;
       if (importedData) {
         var rows = importedData.split(/\r?\n/);
         var importedNotes = [];
         for (var i = 1; i < rows.length; i++) {
           var row = rows[i].trim();
-          if (row !== '') {
+          if (row !== "") {
             var cells = row.split(",");
-            var noteId = cells[0].replace(/"/g, '');
-            var noteContent = cells[1].replace(/"/g, '');
-            var noteColor = cells[2].replace(/"/g, '');
-            importedNotes.push({ id: noteId, content: noteContent, color: noteColor });
+            var noteId = cells[0].replace(/"/g, "");
+            var noteContent = cells[1].replace(/"/g, "");
+            var noteColor = cells[2].replace(/"/g, "");
+            importedNotes.push({
+              id: noteId,
+              content: noteContent,
+              color: noteColor,
+            });
           }
         }
         resolve(importedNotes);
       } else {
-        reject(new Error('No data found in the CSV file.'));
+        reject(new Error("No data found in the CSV file."));
       }
     };
-    reader.onerror = function() {
-      reject(new Error('Error occurred while reading the CSV file.'));
+    reader.onerror = function () {
+      reject(new Error("Error occurred while reading the CSV file."));
     };
     reader.readAsText(file);
   });
-};
-
-
+}
 
 // Attach event listener to export button
-exportButton.addEventListener('click', function() {
+exportButton.addEventListener("click", function () {
   exportNotesAsCSV();
 });
 
 // Attach event listener to import button
-importButton.addEventListener('click', function() {
+importButton.addEventListener("click", function () {
   importFile.click();
 });
 
-
-
 // Function to save note to local storage
 function saveNoteToLocalStorage(noteId, noteContent, noteColor) {
-  return new Promise(function(resolve) {
+  return new Promise(function (resolve) {
     var noteData = {
       content: noteContent,
       color: noteColor,
@@ -274,37 +263,32 @@ function saveNoteToLocalStorage(noteId, noteContent, noteColor) {
     localStorage.setItem(noteKeyPrefix + noteId, JSON.stringify(noteData));
     resolve();
   });
-};
-
-
-
+}
 
 // Attach event listener to import file input
-importFile.addEventListener('change', function(event) {
+importFile.addEventListener("change", function (event) {
   var file = event.target.files[0];
   importNotesFromCSV(file)
-    .then(function(importedNotes) {
+    .then(function (importedNotes) {
       // Import notes
-      importedNotes.forEach(function(note) {
+      importedNotes.forEach(function (note) {
         var noteId = note.id;
         var noteContent = note.content;
         var noteColor = note.color;
         var noteItem = createNoteItem(noteId, noteContent, noteColor);
-        document.getElementById('note-list').appendChild(noteItem);
-        saveNoteToLocalStorage(noteId, noteContent, noteColor).then(function() {
-          console.log('Note imported and saved:', noteContent);
-        });
+        document.getElementById("note-list").appendChild(noteItem);
+        saveNoteToLocalStorage(noteId, noteContent, noteColor).then(
+          function () {
+            console.log("Note imported and saved:", noteContent);
+          }
+        );
       });
-      console.log('All notes imported successfully.');
+      console.log("All notes imported successfully.");
     })
-    .catch(function(error) {
-      console.error('Error importing notes:', error);
+    .catch(function (error) {
+      console.error("Error importing notes:", error);
     });
 });
-
-
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
   // Load all saved notes from local storage
@@ -339,23 +323,19 @@ function getFormattedDate() {
 }
 
 document.getElementById("clear-button").addEventListener("click", function () {
-  $('#clearModal').modal('show');
-  var clearAllNotesButton = document.getElementById('clearAll');
-  clearAllNotesButton.addEventListener('click', function () {
-    
-      clearAllNotes();
-    
-    $('#clearModal').modal('hide');
+  $("#clearModal").modal("show");
+  var clearAllNotesButton = document.getElementById("clearAll");
+  clearAllNotesButton.addEventListener("click", function () {
+    clearAllNotes();
+
+    $("#clearModal").modal("hide");
   });
 });
 
-
-
-
 function clearAllNotes() {
   // Clear all notes from the note list
-  var noteList = document.getElementById('note-list');
-  noteList.innerHTML = '';
+  var noteList = document.getElementById("note-list");
+  noteList.innerHTML = "";
 
   // Retrieve all note keys with the "note_" prefix
   var noteKeys = [];
@@ -367,20 +347,19 @@ function clearAllNotes() {
   }
 
   // Asynchronously remove notes from local storage
-  var removalPromises = noteKeys.map(function(key) {
-    return new Promise(function(resolve) {
+  var removalPromises = noteKeys.map(function (key) {
+    return new Promise(function (resolve) {
       localStorage.removeItem(key);
       resolve();
     });
   });
 
   // Wait for all removal promises to resolve
-  Promise.all(removalPromises).then(function() {
-    console.log('All notes cleared successfully.');
+  Promise.all(removalPromises).then(function () {
+    console.log("All notes cleared successfully.");
     // Perform any additional actions after all notes are cleared
   });
 }
-
 
 // function clearAllNotes() {
 //   // Clear all notes from local storage
@@ -628,25 +607,23 @@ document
     getRandomAffirmation();
   });
 
+var colorButtons = document.querySelectorAll(".filter-button");
 
-  var colorButtons = document.querySelectorAll('.filter-button');
+colorButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    var selectedColor = button.getAttribute("data-color");
+    console.log(selectedColor);
 
-  colorButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
-      var selectedColor = button.getAttribute('data-color');
-      console.log(selectedColor);
-      
-      var noteItems = document.querySelectorAll('.list-group-item');
-      noteItems.forEach(function(note) {
-        var noteColor = note.style.backgroundColor; // Set the note item background color
-        noteColor = rgbToHex(noteColor);
-        console.log(noteColor);
-        if (selectedColor === 'all' || noteColor === selectedColor) {
-          note.classList.remove('hide');
-        } else {
-          note.classList.add('hide');
-        }
-      });
+    var noteItems = document.querySelectorAll(".list-group-item");
+    noteItems.forEach(function (note) {
+      var noteColor = note.style.backgroundColor; // Set the note item background color
+      noteColor = rgbToHex(noteColor);
+      console.log(noteColor);
+      if (selectedColor === "all" || noteColor === selectedColor) {
+        note.classList.remove("hide");
+      } else {
+        note.classList.add("hide");
+      }
     });
   });
-  
+});
